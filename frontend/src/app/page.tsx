5594b49755465644a22dac777a44dffad7364fdb
Matt J Raibert <mattraibert@positiondev.com>
2024-04-05 06:01:09 -0400

    user supply search query

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -21,12 +21,16 @@ type result = [
 export default function Home() {
   // search result state
   const [searchResults, setSearchResults] = useState<result[]>([])
+  const [searchQuery, setSearchQuery] = useState('')
   return (
     <main>
-      <input type="text" placeholder="Search" />
+      <input type="text" placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
       <button
         onClick={async () => {
-          const res = await fetch('/api/search?q=%22what%20is%20a%20propaganda%20book?%22&num=10')
+          const url = new URL(`http://localhost:3000/api/search`)
+          url.searchParams.set('q', searchQuery)
+          url.searchParams.set('num', '100')
+          const res = await fetch(url.toString())
 
           const data = await res.json()
 
@@ -41,7 +45,9 @@ export default function Home() {
         <ul>
           {searchResults.map(([result, score], i) => (
             <li key={i}>
-              <h3>{result.metadata.title}</h3>
+              <h3>
+                [{result.metadata.id}] {result.metadata.title} ({score})
+              </h3>
               <p>{result.pageContent}</p>
             </li>
           ))}
